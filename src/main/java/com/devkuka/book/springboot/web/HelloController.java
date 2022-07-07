7368a4001684786b64d0d9cd1bfd41372460f16c
Kyungtae Bae <Kyungtae@marvrus.com>
2022-07-07 21:14:28 +0900

    챕터2 완료

---
+++ src/main/java/com/devkuka/book/springboot/web/HelloController.java
@@ -0,0 +1,20 @@
+package com.devkuka.book.springboot.web;
+
+import com.devkuka.book.springboot.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello/dto")
+    public String hello(){
+        return "hello";
+    }
+
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
+}
---
+++ src/test/java/com/devkuka/book/springboot/web/HelloControllerTest.java
@@ -0,0 +1,59 @@
+package com.devkuka.book.springboot.web;
+
+import com.devkuka.book.springboot.web.dto.HelloResponseDto;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello가_리턴된다() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void 롬복_기능_테스트() throws Exception {
+        //given
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "test";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk());
+
+    }
+}
